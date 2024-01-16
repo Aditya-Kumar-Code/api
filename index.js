b3e687b70823bff73c874b196d161fffe8a31ecd
Aditya Kumar <87271653+Aditya-Kumar-Code@users.noreply.github.com>
2024-01-17 02:48:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require('express')
 const app = express()
 const PORT = 4000
 
+const cors = require("cors");
+app.use(cors());
 app.listen(PORT, () => {
   console.log(`API listening on PORT ${PORT} `)
 })
